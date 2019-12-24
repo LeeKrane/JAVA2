72d4eb50e35a58f7b806979af20f8267c61017b4
LeeKrane <godfist.leekrane@gmail.com>
2019-12-24 01:13:54 +0100

    - solved Labor04

---
+++ source/labors/labor04/PortalCrane.java
@@ -0,0 +1,80 @@
+package labors.labor04;
+
+import java.util.Arrays;
+
+public class PortalCrane {
+	/* private */ int[] silos;
+	/* private */ int maximumFillValue;
+	/* private */ int position;
+	
+	public static void main (String[] args) {
+		PortalCrane crane = new PortalCrane(5, 10);
+		crane.fill(4);
+		crane.fill(1);
+		crane.moveRight(2);
+		crane.fill(20);
+		crane.moveRight(2);
+		crane.fill(1);
+		crane.moveLeft(42);
+		int content = crane.dropAll();
+		crane.moveRight(1);
+		crane.fill(content);
+		System.out.println(crane);
+	}
+	
+	public PortalCrane (int silos, int maximumFillValue) {
+		this.silos = new int[silos];
+		this.maximumFillValue = maximumFillValue;
+	}
+	
+	void moveLeft (int i) {
+		if (position - i < 0)
+			i = position;
+		position -= i;
+	}
+	
+	void moveRight (int i) {
+		if (position + i >= silos.length)
+			i = silos.length - position - 1;
+		position += i;
+	}
+	
+	void fill (int x) {
+		if (silos[position] + x > maximumFillValue)
+			x = maximumFillValue - silos[position];
+		else if (x < 0)
+			x = 0;
+		silos[position] += x;
+	}
+	
+	int dropAll () {
+		int content = silos[position];
+		silos[position] = 0;
+		return content;
+	}
+	
+	@Override
+	public String toString () {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < silos.length; i++) {
+			builder.append("silo ").append(i + 1).append(": ").append(silos[i]);
+			if (position == i)
+				builder.append(" (current position)");
+			builder.append('\n');
+		}
+		return builder.toString();
+	}
+	
+	// fancy methods
+	void fancyMoveLeft (int i) {
+		position -= -Math.max(-position, -i);
+	}
+	
+	void fancyMoveRight (int i) {
+		position += Math.min(silos.length - 1 - position, i);
+	}
+	
+	void fancyFill (int x) {
+		silos[position] += x >= 0 ? Math.min(maximumFillValue - silos[position], x) : 0;
+	}
+}
---
+++ tests/labors/labor04/PortalCraneTest.java
@@ -0,0 +1,117 @@
+package labors.labor04;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PortalCraneTest {
+	@Test
+	public void mainTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		assertEquals(5, crane.silos.length);
+		assertEquals(10, crane.maximumFillValue);
+		assertEquals(0, crane.position);
+		crane.fill(4);
+		assertEquals(4, crane.silos[0]);
+		crane.fill(1);
+		assertEquals(5, crane.silos[0]);
+		crane.moveRight(2);
+		assertEquals(2, crane.position);
+		crane.fill(20);
+		assertEquals(10, crane.silos[2]);
+		crane.moveRight(2);
+		assertEquals(4, crane.position);
+		crane.fill(1);
+		assertEquals(1, crane.silos[4]);
+		crane.moveLeft(42);
+		assertEquals(0, crane.position);
+		int content = crane.dropAll();
+		assertEquals(5, content);
+		assertEquals(0, crane.silos[0]);
+		crane.moveRight(1);
+		assertEquals(1, crane.position);
+		crane.fill(content);
+		assertEquals(5, crane.silos[1]);
+		String expected = "silo 1: 0\nsilo 2: 5 (current position)\nsilo 3: 10\nsilo 4: 0\nsilo 5: 1\n";
+		assertEquals(expected, crane.toString());
+	}
+	
+	@Test
+	public void constructorTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		assertEquals(5, crane.silos.length);
+		assertEquals(10, crane.maximumFillValue);
+		assertEquals(0, crane.position);
+	}
+	
+	@Test
+	public void moveTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		assertEquals(0, crane.position);
+		crane.moveLeft(69);
+		assertEquals(0, crane.position);
+		crane.moveRight(69);
+		assertEquals(4, crane.position);
+		crane.moveLeft(3);
+		assertEquals(1, crane.position);
+		crane.moveRight(2);
+		assertEquals(3, crane.position);
+	}
+	
+	@Test
+	public void fillTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		crane.fill(69);
+		assertEquals(10, crane.silos[0]);
+		crane.moveRight(1);
+		crane.fill(-69);
+		assertEquals(0, crane.silos[1]);
+		crane.fill(6);
+		assertEquals(6, crane.silos[1]);
+		crane.fill(-3);
+		assertEquals(6, crane.silos[1]);
+		crane.fill(420);
+		assertEquals(10, crane.silos[1]);
+	}
+	
+	@Test
+	public void dropAllTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		crane.fancyFill(5);
+		assertEquals(5, crane.dropAll());
+		assertEquals(0, crane.silos[0]);
+	}
+	
+	// fancy tests
+	@Test
+	public void fancyMoveTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		assertEquals(0, crane.position);
+		crane.fancyMoveLeft(69);
+		assertEquals(0, crane.position);
+		crane.fancyMoveRight(69);
+		assertEquals(4, crane.position);
+		crane.fancyMoveLeft(3);
+		assertEquals(1, crane.position);
+		crane.fancyMoveRight(2);
+		assertEquals(3, crane.position);
+	}
+	
+	@Test
+	public void fancyFillTest () {
+		PortalCrane crane = new PortalCrane(5, 10);
+		crane.fancyFill(-69);
+		assertEquals(0, crane.silos[0]);
+		crane.fancyFill(69);
+		assertEquals(10, crane.silos[0]);
+		crane.fancyMoveRight(1);
+		crane.fancyFill(-69);
+		assertEquals(0, crane.silos[1]);
+		crane.fancyFill(6);
+		assertEquals(6, crane.silos[1]);
+		crane.fancyFill(-3);
+		assertEquals(6, crane.silos[1]);
+		crane.fancyFill(420);
+		assertEquals(10, crane.silos[1]);
+	}
+}
